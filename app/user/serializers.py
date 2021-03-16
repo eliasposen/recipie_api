dda29f58a34ee79d73204a49353083e171b594f6
Elias Posen <elias.posen@eigentech.com>
2021-03-16 17:22:54 +0000

    me/ user endpoint + tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,6 +18,18 @@ class UserSerializer(serializers.ModelSerializer):
         """Creates new user with encrypted password & return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password and return it"""
+        password = validated_data.pop('password', None)
+
+        user = super().update(instance, validated_data)
+
+        if password is not None:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for user authentication object"""
